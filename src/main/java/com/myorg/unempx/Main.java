c3d3d661c81714b23f3acc6dd71d60e766cbae34
Nikolaspc <n.perez.cvjetkovic@gmail.com>
2025-09-17 16:34:32 +0200

    Agrega logging profesional SLF4J + Logback, ajustes de Main y pom.xml

--- src/main/java/com/myorg/unempx/Main.java
+++ src/main/java/com/myorg/unempx/Main.java
@@ -1,10 +1,25 @@
 package com.myorg.unempx;
 
 import com.myorg.unempx.ui.UnempXApp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new UnempXApp().setVisible(true));
+        log.info("Launching UnempX Swing application...");
+
+        try {
+            SwingUtilities.invokeLater(() -> {
+                log.debug("Initializing UnempXApp UI...");
+                new UnempXApp().setVisible(true);
+                log.info("UnempXApp UI is now visible.");
+            });
+        } catch (Exception e) {
+            log.error("Unexpected error while starting UnempX application", e);
+        }
     }
 }
